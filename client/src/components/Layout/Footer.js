9ad0db275564c9284244aa7c51ea817b455544bd
Anand Kumar Saw <105969014+Anixanand@users.noreply.github.com>
2023-07-01 01:21:52 +0530

    first commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-light p-4">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; Anand
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
